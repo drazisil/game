b7a96c3b7017ae39097855252779bc4c799331c0
Joe Becher <jwbecher@drazisil.com>
2019-06-19 18:10:52 -0400

    orginize assets start creating missiles

--- index.js
+++ index.js
@@ -3,15 +3,35 @@ let containerId = "game"
 let backgroundLoaded = false
 let shipLoaded = false
 const gameConfig = {
-    movementUnit: 5
+    movementUnit: 5,
+    width: 800,
+    height: 600
 }
 const gameState = {
     shipLocation: {x: 100, y: 100},
-    spaceLocation: {x : -500, y: -500}
+    spaceLocation: {x : -500, y: -500},
+    missiles: []
+}
+
+// [{ x: number, y: number }]
+
+function drawMissile(missile) {
+    console.log(`Drawing missile`)
+    let x = (missile.x  + (gameObjects.ship.width / 4))
+    ctx.strokeStyle = "red"
+    ctx.beginPath(); 
+    ctx.moveTo(x, missile.y);    // Move the pen to (30, 50)
+    ctx.lineTo(x, missile.y - 20);  // Draw a line to (150, 100)
+    ctx.lineWidth = 2;
+    ctx.stroke();
+}
+
+function moveLeft(params) {
+    
 }
 
 function dealWithKeyboard(e) {
-    const { type, key } = e
+    const { type, key, code } = e
     const { movementUnit } = gameConfig
 
 
@@ -44,8 +64,25 @@ function dealWithKeyboard(e) {
         return
     }
 
+    if(type === 'keydown' && code === 'Space') {
+        gameState.missiles.push({
+            x: gameState.shipLocation.x, 
+            y: gameState.shipLocation.y
+        })
+        e.preventDefault()
+        return
+    }
+
+    if (type === 'keyup' && code === 'Space') {
+        gameState.missiles = gameState.missiles.filter((missile) => {
+            return missile.x !== gameState.shipLocation.x
+        })
+        e.preventDefault()
+        return
+    }
+
     e.preventDefault()
-    // console.log(`${type} detected on key ${key}`)
+    console.log(`${type} detected on key (${key}) / (${code})`)
 }
 
 
@@ -53,8 +90,7 @@ function dealWithKeyboard(e) {
 window.addEventListener("keypress", dealWithKeyboard, false);
 window.addEventListener("keyup", dealWithKeyboard, false);
 
-    const width = 600
-    const height = 600
+
 
     const ship = document.images[0]
     const space = document.images[2]
@@ -62,8 +98,8 @@ window.addEventListener("keyup", dealWithKeyboard, false);
     let body = document.getElementById(containerId)
     let canvas = document.createElement("canvas")
     canvas.setAttribute("id", "canvas")
-    canvas.setAttribute("width", width)
-    canvas.setAttribute("height", height)
+    canvas.setAttribute("width", gameConfig.width)
+    canvas.setAttribute("height", gameConfig.height)
     body.appendChild(canvas)
     
     const ctx = canvas.getContext('2d');
@@ -99,8 +135,12 @@ function run() {
     }
     // console.log('Game tick')
     // console.log(gameState)
+    ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
     ctx.drawImage(gameObjects.space, gameState.spaceLocation.x, gameState.spaceLocation.y, gameObjects.space.width / 2, gameObjects.space.height / 2)
     ctx.drawImage(gameObjects.ship, gameState.shipLocation.x, gameState.shipLocation.y, gameObjects.ship.width /2, gameObjects.ship.height / 2)
+    gameState.missiles.forEach((missile) => {
+        drawMissile(missile)
+    })
     request = window.requestAnimationFrame(run);
 
 }
