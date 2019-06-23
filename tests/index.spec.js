fbd68f758c3c8cda0c862f6077f3e60e554f0380
Joe Becher <jwbecher@drazisil.com>
2019-06-23 15:28:47 -0400

    add some more tests

--- tests/index.spec.js
+++ tests/index.spec.js
@@ -1,7 +1,7 @@
 const { GameObject, Missile, Game } = require("../src/app.js");
 
-describe("GameObject", () => {
-  test("Can create a GameObject", () => {
+describe("with GameObject", () => {
+  test("that can create a GameObject", () => {
     const gameObject = new GameObject(
       "dummyObject",
       null,
@@ -15,21 +15,43 @@ describe("GameObject", () => {
   });
 });
 
-describe("Missile", () => {
-  test("Can create a Missile", () => {
+describe("with Missile", () => {
+  test("that can create a Missile", () => {
     const missile = new Missile(14, 24, 16, "dummySource");
     expect(missile.source).toEqual("dummySource");
   });
 });
 
-describe("Game", () => {
+describe("with Game", () => {
   let game;
 
   beforeEach(() => {
     game = new Game();
   });
 
-  test("ship can move up", () => {
+  test("that areEnemiesLoaded() returns false with enemies not loaded", () => {
+    game.gameObjects.enemies[0] = { data: { complete: false } };
+    expect(game.areEnemiesLoaded()).toEqual(false);
+  });
+
+  test("that areEnemiesLoaded() returns true with enemies loaded", () => {
+    game.gameObjects.enemies[0] = { data: { complete: true } };
+    expect(game.areEnemiesLoaded()).toEqual(true);
+  });
+
+  test("that resetSizes() corrects width and height", () => {
+    game.gameObjects.ship = {
+      data: { width: 40, height: 80 },
+      width: 0,
+      height: 0
+    };
+    game.gameObjects.enemies[0] = { data: { complete: true } };
+    game.resetSizes();
+    expect(game.gameObjects.ship.width).toEqual(20);
+    expect(game.gameObjects.ship.height).toEqual(40);
+  });
+
+  test("that ship can move up", () => {
     game.gameObjects.ship = { y: 700 };
     game.gameObjects.ship.y = game.gameConfig.defaultShipLocation.y;
     game.moveUp();
@@ -38,7 +60,7 @@ describe("Game", () => {
     );
   });
 
-  test("ship can move down", () => {
+  test("that ship can move down", () => {
     game.gameObjects.ship = { y: 700 };
     game.gameObjects.ship.y = game.gameConfig.defaultShipLocation.y;
     game.moveDown();
@@ -47,7 +69,7 @@ describe("Game", () => {
     );
   });
 
-  test("ship can move left", () => {
+  test("that ship can move left", () => {
     game.gameObjects.ship = { x: 42 };
     game.gameObjects.ship.x = game.gameConfig.defaultShipLocation.x;
     game.moveLeft();
@@ -56,7 +78,7 @@ describe("Game", () => {
     );
   });
 
-  test("ship can move right", () => {
+  test("that ship can move right", () => {
     game.gameObjects.ship = { x: 42 };
     game.gameObjects.ship.x = game.gameConfig.defaultShipLocation.x;
     game.moveRight();
